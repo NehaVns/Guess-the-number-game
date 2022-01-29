48574e0597c420c4673ee44cb084ab765e2ce17d
NehaVns <nehavns2848@gmail.com>
2022-01-29 09:53:18 +0530

    Initial Commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,27 @@
+int number,guess,min,max,count=1;
+//     printf("Enter the minimum limit : ");
+//     scanf("%d",&min);
+//     printf("Enter the maximum limit : ");
+//     scanf("%d",&max);
+//     srand(time(0));
+//     number=rand()%(max-min+1)+min;
+//     //printf("The value of random number is : %d\n",number);
+//     do
+//     {
+//         printf("Enter a number between %d and %d : ",min,max);
+//         scanf("%d",&guess);
+//         if (guess<number)
+//         {
+//             printf("Greater number please !\n");
+//         }
+//         else if (guess>number)
+//         {
+//             printf("Lower number please !\n");
+//         }
+//         else{
+//             printf("You guessed it right !\nTotal attempts = %d",count);
+//         }
+//         count++;   
+//     } while (guess!=number);
+//     return 0;
+// }
